69f36457bf7460703b4993f2e58a063bf7438563
lmq <liumeiqin@yidianlife.com>
2022-09-01 23:17:39 +0800

    配置

---
+++ babel.config.js
@@ -0,0 +1,5 @@
+module.export = {
+    preset: [
+        'react-app'
+    ]
+}
--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,44 +1,112 @@
 const path = require('path')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const ESlintPlugin = require('eslint-webpack-plugin')
+
+function getStyleLoaders(val) {
+    return  [
+        MiniCssExtractPlugin.loader,
+       'css-loader',
+      //  css兼容性配置，需要再package.json文件配置浏览器兼容成度
+       {
+          loader: 'postcss-loader',
+          options: {
+              postcssOptions: {
+                  plugins: [
+                      'postcss-preset-env',
+                  ]
+              }
+          }
+       },
+       val,
+   ].filter(Boolean) // 清除空数组
+}
 
 module.export = {
     entry: './src/main.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'static/js/[contenthash].js',
-        chunkFilename: 'static/js/[contenthash].chunk.js',
-        assetModuleFinename: 'static/media/[contenthash].chunk.js',
+        filename: 'static/js/[contenthash:10].js',
+        chunkFilename: 'static/js/[contenthash:10].chunk.js',
+        assetModuleFinename: 'static/media/[contenthash:10][ext][query]',
     },
     module: {
         rules: [
+            // css处理: 兼容性，压缩
            {
-             // css处理: 兼容性，压缩
+             test: /\.css$/,
+             use: getStyleLoaders()
+           },
+           {
              test: /\.less$/,
-             use: [
-                  MiniCssExtractPlugin.loader,
-                 'css-loader',
-                 {
-                    loader: 'postcss-loader',
-                    options: {
-                        postcssOptions: {
-                            plugins: [
-                                'postcss-preset-env',
-                            ]
-                        }
-                    }
-                 },
-                 'less-loader',
-             ]
-           }
-
-        
+             use: getStyleLoaders('less-loader')
+           },
+           {
+             test: /\.s[ac]ss$/,
+             use: getStyleLoaders('sass-loader')
+           },
+           {
+             test: /\.styl$/,
+             use: getStyleLoaders('stylus-loader')
+           },
             // 图片，字体图标处理
+            {
+                test: /\.jpe?j|gif|webp|png|svg$/,
+                type: 'asset',
+                parser: {
+                    dataUrlCondition: {
+                        maxSize: 10 * 1024
+                    }
+                }
+
+            },
+            {
+                test: /\.ttf|woff[2]?$/,
+                type: 'asset/source'
+            },
             // js处理：eslint,babel,压缩
+            // babel需要配合babel.config.js文件进行配置
+            {
+                test: /\.jsx?$/,
+                exclude: 'node_modules',
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        cacheDirectory: true,
+                        cacheCompression: false,
+                    }
+                }
+            }
 
         ]
     },
     plugins: [
-        new MiniCssExtractPlugin()
+        new MiniCssExtractPlugin(),
+        new ESlintPlugin({
+            context: path.resolve(__dirname, '../src'),
+            exclude: 'node_modules',
+            cache: true,
+            cacheLocation: path.resolve(__dirname, '../node_modules/.cache/eslintcache')
+        }),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, '../publc/index.html')
+        })
     ],
     mode: 'development',
+    devtool: 'cheap-module-source-map',
+    // 代码分割打包
+    optimization: {
+        splitChunks: {
+            chunks: 'all',
+        },
+        runtimeChunk: {
+            name: (entrypoint) => `runtime~~${entrypoint.name}.js`
+        }
+    },
+    // 开发环境配置
+    devServer: {
+        host: 'localhost',
+        prot: 3000,
+        open: true,
+        hot: true
+    }
 }
